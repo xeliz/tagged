80d7ca3d0fc3d83a9820e77170b924a85ba77a24
xeliz <szczanpasz@gmail.com>
2019-01-06 22:30:19 +0300

    small changes in templates

--- app.py
+++ app.py
@@ -95,13 +95,14 @@ def login_user(userid):
 # - update session table if the param is True
 # - redirect to login page
 def unlogin_user(deactivateSession=True):
-    token = flask.request.cookies.get("session_token")
-    if deactivateSession:
-        query = """UPDATE sessions SET active = FALSE where id = %s"""
-        cur.execute(query, (token,))
-        con.commit()
     resp = flask.make_response(flask.redirect(flask.url_for("login_page")))
-    resp.set_cookie("session_token", "", expires=0)
+    if "session_token" in flask.request.cookies:
+        token = flask.request.cookies.get("session_token")
+        if deactivateSession:
+            query = """UPDATE sessions SET active = FALSE where id = %s"""
+            cur.execute(query, (token,))
+            con.commit()
+        resp.set_cookie("session_token", "", expires=0)
     return resp
 
 # create flask app
@@ -145,23 +146,23 @@ def index_page():
     result = cur.execute("SELECT id,title,contents,date_created,tags,date_modified FROM notes ORDER BY date_modified DESC LIMIT 10");
     notes = fetchall_as_dict(cur)
     # notes = cur.fetchall()
-    return flask.render_template("index.html", title="Главная", notes=notes)
+    return flask.render_template("index.html", notes=notes)
 
 # new note page: add new note
 @app.route("/new", methods=["GET", "POST"])
 def new_page():
     if flask.request.method == "GET":
-        return flask.render_template("new.html", title="Новая запись")
+        return flask.render_template("new.html")
     elif flask.request.method == "POST":
         if not contains_all(flask.request.form, ("title", "contents", "tags")):
-            return flask.render_template("message.html", title="Сообщение", message="Неверный запрос")
+            return flask.render_template("message.html", message="Неверный запрос")
         title = html.escape(flask.request.form.get("title", ""))
         contents = html.escape(flask.request.form.get("contents", "")).replace("\n", "<br>")
         rawTags = html.escape(flask.request.form.get("tags", "")).strip()
         tagList = rawTags.split()
         for tag in tagList:
             if not validate_tag(tag):
-                return flask.render_template("message.html", title="Сообщение", message="Ошибка: метка может содержать только буквы, цифры, нижнее подчёркивание (_) или дефис(-)")
+                return flask.render_template("message.html", message="Ошибка: метка может содержать только буквы, цифры, нижнее подчёркивание (_) или дефис(-)")
         tags = " ".join(tagList)
         curdt = datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d %H:%M:%S")
         query = "INSERT INTO notes(title,contents,date_created,tags,date_modified,userid)VALUES(%s,%s,%s,%s,%s,1)"
@@ -169,19 +170,19 @@ def new_page():
         con.commit()
         return flask.redirect(flask.url_for("note_noteid_page", noteid=cur.lastrowid))
     else:
-        return flask.render_template("message.html", title="Сообщение", message="Метод не поддерживается")
+        return flask.render_template("message.html", message="Метод не поддерживается")
 
 # all notes page
 @app.route("/all")
 def all_page():
     result = cur.execute("SELECT id,title,contents,date_created,tags,date_modified FROM notes ORDER BY date_modified DESC");
     notes = fetchall_as_dict(cur)
-    return flask.render_template("all.html", title="Все записи", notes=notes)
+    return flask.render_template("all.html", notes=notes)
 
 # search page
 @app.route("/search")
 def search_page():
-    return flask.render_template("search.html", title="Поиск")
+    return flask.render_template("search.html")
 
 # search results page
 @app.route("/search/results")
@@ -189,15 +190,15 @@ def search_results_page():
     rawKeywords = flask.request.args.get("keywords", "").strip()
     rawTags = flask.request.args.get("tags", "").strip()
     if rawKeywords == "" and rawTags == "":
-        return flask.render_template("message.html", title="Сообщение", message="Неправильные параметры поиска")
+        return flask.render_template("message.html", message="Неправильные параметры поиска")
     keywords = rawKeywords.split()
     tags = rawTags.split()
     for tag in tags:
         if not validate_tag(tag):
-            return flask.render_template("message.html", title="Сообщение", message="Неправильные параметры поиска")
+            return flask.render_template("message.html", message="Неправильные параметры поиска")
     for keyword in keywords :
         if not validate_tag(keyword):
-            return flask.render_template("message.html", title="Сообщение", message="Неправильные параметры поиска")
+            return flask.render_template("message.html", message="Неправильные параметры поиска")
     keywordsLike = list(map(lambda k: ["%" + k + "%"]*2, keywords))
     keywordsLike1 = []
     for keywordLike in keywordsLike:
@@ -214,20 +215,20 @@ def search_results_page():
     query = "".join(queryParts)
     cur.execute(query, keywordsLike + tagsLike)
     notes = fetchall_as_dict(cur)
-    return flask.render_template("search_results.html", title="Результаты поиска", keywords=keywords, tags=tags, notes=notes)
+    return flask.render_template("search_results.html", keywords=keywords, tags=tags, notes=notes)
 
 # delete page
 @app.route("/delete/<int:noteid>", methods=["GET", "POST"])
 def delete_noteid_page(noteid):
     if flask.request.method == "GET":
-        return flask.render_template("delete_noteid.html", title="Удаление записи", noteid=noteid)
+        return flask.render_template("delete_noteid.html", noteid=noteid)
     elif flask.request.method == "POST":
         query = "DELETE FROM notes WHERE id={}".format(noteid)
         cur.execute(query)
         con.commit()
-        return flask.render_template("message.html", title="Сообщение", message="Удалено")
+        return flask.render_template("message.html", message="Удалено")
     else:
-        return flask.render_template("message.html", title="Сообщение", message="Метод не поддерживается")
+        return flask.render_template("message.html", message="Метод не поддерживается")
 
 # specific note page
 @app.route("/note/<int:noteid>")
@@ -235,7 +236,7 @@ def note_noteid_page(noteid):
     query = "SELECT id,title,tags,contents,date_created,date_modified FROM notes WHERE id={}".format(noteid)
     cur.execute(query)
     note = fetchone_as_dict(cur)
-    return flask.render_template("note_noteid.html", title="Просмотр записи", note=note)
+    return flask.render_template("note_noteid.html", note=note)
 
 # note editing page
 @app.route("/edit/<int:noteid>", methods=["GET", "POST"])
@@ -245,17 +246,17 @@ def edit_noteid_page(noteid):
         cur.execute(query)
         note = fetchone_as_dict(cur)
         note["contents"] = note["contents"].replace("<br>", "\n")
-        return flask.render_template("edit_noteid.html", title="Изменение записи", note=note)
+        return flask.render_template("edit_noteid.html", note=note)
     elif flask.request.method == "POST":
         if not contains_all(flask.request.form, ("title", "contents", "tags")):
-            return flask.render_template("message.html", title="Сообщение", message="Метод не поддерживается")
+            return flask.render_template("message.html", message="Метод не поддерживается")
         title = html.escape(flask.request.form.get("title", ""))
         contents = html.escape(flask.request.form.get("contents", "")).replace("\n", "<br>")
         rawTags = html.escape(flask.request.form.get("tags", "")).strip()
         tagList = rawTags.split()
         for tag in tagList:
             if not validate_tag(tag):
-                return flask.render_template("message.html", title="Сообщение", message="Ошибка: метка может содержать только буквы, цифры, нижнее подчёркивание (_) или дефис(-)")
+                return flask.render_template("message.html", message="Ошибка: метка может содержать только буквы, цифры, нижнее подчёркивание (_) или дефис(-)")
         tags = " ".join(tagList)
         curdt = datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d %H:%M:%S")
         query = "UPDATE notes SET title=%s,contents=%s,tags=%s,date_modified=%s WHERE id={}".format(noteid)
@@ -263,7 +264,7 @@ def edit_noteid_page(noteid):
         con.commit()
         return flask.redirect(flask.url_for("note_noteid_page", noteid=noteid))
     else:
-        return flask.render_template("message.html", title="Сообщение", message="Метод не поддерживается")
+        return flask.render_template("message.html", message="Метод не поддерживается")
 
 # sign in page
 @app.route("/login", methods=["GET", "POST"])
@@ -272,31 +273,31 @@ def login_page():
         return flask.redirect(flask.url_for("profile_page"))
 
     if flask.request.method == "GET":
-        return flask.render_template("login.html", title="Вход")
+        return flask.render_template("login.html")
     elif flask.request.method == "POST":
         if not contains_all(flask.request.form, ("username", "password")):
-            return flask.render_template("unauthorizedmessage.html", title="Сообщение", message="Неправильный запрос")
+            return flask.render_template("unauthorizedmessage.html", message="Неправильный запрос")
 
         username = flask.request.form.get("username")
         if not validate_username(username):
-            return flask.render_template("unauthorizedmessage.html", title="Сообщение", message="Имя пользователя может состоять только из букв, цифр, дефиса (-) или нижнего подчёркивания (_)")
+            return flask.render_template("unauthorizedmessage.html", message="Имя пользователя может состоять только из букв, цифр, дефиса (-) или нижнего подчёркивания (_)")
         # check if user exists
         query = """SELECT id, passhash FROM users WHERE username = %s"""
         cur.execute(query, (username,))
         userdata = fetchall_as_dict(cur)
         if not userdata:
-            return flask.render_template("unauthorizedmessage.html", title="Сообщение", message="Такой пользователь не зарегистрирован")
+            return flask.render_template("unauthorizedmessage.html", message="Такой пользователь не зарегистрирован")
         userdata = userdata[0]
 
         # check if passwords match
         password = flask.request.form.get("password")
         passhash = base64.b64encode(hashlib.md5(password.encode("UTF-8")).digest()).decode("UTF-8")
         if userdata["passhash"] != passhash:
-            return flask.render_template("unauthorizedmessage.html", title="Сообщение", message="Неверный пароль")
+            return flask.render_template("unauthorizedmessage.html", message="Неверный пароль")
 
         return login_user(userdata["id"]) 
     else:
-        return flask.render_template("unauthorizedmessage.html", title="Сообщение", message="Метод не поддерживается")
+        return flask.render_template("unauthorizedmessage.html", message="Метод не поддерживается")
 
 # profile page
 @app.route("/profile", methods=["GET", "POST"])
@@ -305,42 +306,42 @@ def profile_page():
     if not userdata:
         return unlogin_user(False)
     if flask.request.method == "GET":
-        return flask.render_template("profile.html", title="Профиль", userdata=userdata)
+        return flask.render_template("profile.html", userdata=userdata)
     elif flask.request.method == "POST":
         if "action" not in flask.request.form:
-            return flask.render_template("unauthorizedmessage.html", title="Сообщение", message="Неправильный запрос")
+            return flask.render_template("unauthorizedmessage.html", message="Неправильный запрос")
         action = flask.request.form.get("action")
         if action == "logout":
             return unlogin_user()
         else:
-            return flask.render_template("unauthorizedmessage.html", title="Сообщение", message="Неправильное действие")
+            return flask.render_template("unauthorizedmessage.html", message="Неправильное действие")
     else:
-        return flask.render_template("unauthorizedmessage.html", title="Сообщение", message="Метод не поддерживается")
+        return flask.render_template("unauthorizedmessage.html", message="Метод не поддерживается")
 
 # sign up page
 @app.route("/signup", methods=["GET", "POST"])
 def signup_page():
     if flask.request.method == "GET":
-        return flask.render_template("signup.html", title="Регистрация")
+        return flask.render_template("signup.html")
     elif flask.request.method == "POST":
         if contains_all(flask.request.form, ("username", "password1", "password2")):
-            return flask.render_template("unauthorizedmessage.html", title="Сообщение", message="Неправильный запрос")
+            return flask.render_template("unauthorizedmessage.html", message="Неправильный запрос")
         username = flask.request.form.get("username")
         # check if user exists
         query = """SELECT * FROM users WHERE username = %s"""
         cur.execute(query, (username,))
         if cur.fetchall():
-            return flask.render_template("unauthorizedmessage.html", title="Сообщение", message="Это имя пользователя занято")
+            return flask.render_template("unauthorizedmessage.html", message="Это имя пользователя занято")
 
         # check if passwords match
         password1 = flask.request.form.get("password1")
         password2 = flask.request.form.get("password2")
         if password1 != password2:
-            return flask.render_template("unauthorizedmessage.html", title="Сообщение", message="Пароли не совпадают")
+            return flask.render_template("unauthorizedmessage.html", message="Пароли не совпадают")
         passhash = base64.b64encode(hashlib.md5(password1.encode("UTF-8")).digest()).decode("UTF-8")
         query = """INSERT INTO users (username, passhash) VALUES (%s, %s)"""
         cur.execute(query, (username, passhash))
         con.commit()
-        return flask.render_template("unauthorizedmessage.html", title="Сообщение", message="Пользователь зарегистрирован, теперь вы можете авторизоваться")
+        return flask.render_template("unauthorizedmessage.html", message="Пользователь зарегистрирован, теперь вы можете авторизоваться")
     else:
-        return flask.render_template("unauthorizedmessage.html", title="Сообщение", message="Метод не поддерживается")
+        return flask.render_template("unauthorizedmessage.html", message="Метод не поддерживается")
